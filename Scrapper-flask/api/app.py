7954270ab9d9888724dd2380e72853aac47665e0
yash <yash97r@gmail.com>
2022-09-12 20:42:30 +0530

    intergrating with front end

--- Scrapper-flask/api/app.py
+++ Scrapper-flask/api/app.py
@@ -207,4 +207,4 @@ def addVideoToQueue():
     return make_response(jsonify(result),200)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8001)
+    app.run(debug=True,host='0.0.0.0',port=8000)
