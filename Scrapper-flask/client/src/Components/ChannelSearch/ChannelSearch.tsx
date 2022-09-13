7129328bbbc424dee51f91dff9473799a079cafe
yash <yash97r@gmail.com>
2022-09-14 00:58:09 +0530

    chnaging browser router to hash router

--- Scrapper-flask/client/src/Components/ChannelSearch/ChannelSearch.tsx
+++ Scrapper-flask/client/src/Components/ChannelSearch/ChannelSearch.tsx
@@ -17,7 +17,7 @@ const ChannelSearch = (props: any) => {
   const preload = async () => {
     setShowSpinner(true);
     const searchResponse = await fetch(
-      `http://137.184.202.24:80/api/fetch/video_details?channel_id=${id}`
+      `http://localhost:80/api/fetch/video_details?channel_id=${id}`
     );
     if (searchResponse.status !== 200) throw Error("No results found");
     const searchResData = await searchResponse.json();
@@ -101,7 +101,7 @@ const ChannelSearch = (props: any) => {
 
   const downloadVideoToS3 = async (id: any, link: any) => {
     const response = await fetch(
-      `http://137.184.202.24:80/api/queue/scrap/video`,
+      `http://localhost:80/api/queue/scrap/video`,
       {
         method: "POST",
         headers: {
@@ -119,7 +119,7 @@ const ChannelSearch = (props: any) => {
   const downloadVideo = async (id: any) => {
     setShowSpinner(true);
     const response = await fetch(
-      `http://137.184.202.24:80/api/queue/data?data=youtube&id=${id}`
+      `http://localhost:80/api/queue/data?data=youtube&id=${id}`
     );
     if (response.ok) {
       const resData = await response.json();
@@ -130,7 +130,7 @@ const ChannelSearch = (props: any) => {
   const openCommentsModal = async (id: any) => {
     setShowSpinner(true);
     const response = await fetch(
-      `http://137.184.202.24:80/api/queue/data?data=comment&id=${id}`
+      `http://localhost:80/api/queue/data?data=comment&id=${id}`
     );
     if (response.ok) {
       const resData = await response.json();
@@ -161,7 +161,7 @@ const ChannelSearch = (props: any) => {
                     <button
                       onClick={() => {
                         window.location.href =
-                          "http://137.184.202.24:80/ytshomepage";
+                          "http://localhost:80/ytshomepage";
                       }}
                     >
                       ◀BACK
